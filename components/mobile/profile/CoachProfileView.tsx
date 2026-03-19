ce2e20beab83474cf1d11712a5502385abc50360
pomarola99 <pomatifranco@gmail.com>
2026-03-19 11:19:26 -0300

    design: Faster horizontal transition with md:grid-cols-4

--- components/mobile/profile/CoachProfileView.tsx
+++ components/mobile/profile/CoachProfileView.tsx
@@ -171,7 +171,7 @@ export function CoachProfileView({ logic }: CoachProfileViewProps) {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
                     <SocialConnections />
                     <MercadoPagoConnection />
                     <GoogleCalendarConnection />
